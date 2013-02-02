bca11e48ead028f12caf5ae4abd4ee08553133cc
Spencer Salazar <spencer@ccrma.stanford.edu>
2013-02-02 11:30:23 +0000

    fix non-shutdown bug for MAUI/GlucK

--- chuck_vm.cpp
+++ chuck_vm.cpp
@@ -527,7 +527,11 @@ t_CKBOOL Chuck_VM::shutdown()
     SAFE_RELEASE( m_dac );
     SAFE_RELEASE( m_adc );
     SAFE_RELEASE( m_bunghole );
-
+    
+    if( m_main_thread_quit )
+        m_main_thread_quit( m_main_thread_bindle );
+    clear_main_thread_hook();
+    
     m_init = FALSE;
 
     // pop indent
@@ -747,6 +751,10 @@ t_CKBOOL Chuck_VM::run( t_CKINT num_samps )
 // vm stop here
 vm_stop:
     m_running = FALSE;
+    
+    if( m_main_thread_quit )
+        m_main_thread_quit( m_main_thread_bindle );
+    clear_main_thread_hook();
 
     // log
     EM_log( CK_LOG_SYSTEM, "virtual machine stopped..." );
@@ -783,9 +791,6 @@ t_CKBOOL Chuck_VM::stop( )
     m_running = FALSE;
     Digitalio::m_end = TRUE;
     
-    if( m_main_thread_quit )
-        m_main_thread_quit( m_main_thread_bindle );
-
     return TRUE;
 }
 
