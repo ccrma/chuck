2aab2717334d4e8334eae0be18e2304b40bb4a65
Spencer Salazar <spencer@ccrma.stanford.edu>
2013-05-10 10:41:26 +0000

    update version

--- src/chuck_globals.cpp
+++ src/chuck_globals.cpp
@@ -40,7 +40,7 @@
 
 
 // current version
-const char CK_VERSION[] = "1.3.2.0-beta-2 (chimera)";
+const char CK_VERSION[] = "1.3.2.0-beta-3 (chimera)";
 
 // global virtual machine
 Chuck_VM * g_vm = NULL;
