232c2b00dea093443eb49018286fb840294b13e7
Spencer Salazar <spencer@ccrma.stanford.edu>
2013-07-24 17:28:40 +0000

    regex module

--- src/chuck_compile.cpp
+++ src/chuck_compile.cpp
@@ -44,6 +44,7 @@
 #include "ulib_math.h"
 #include "ulib_std.h"
 #include "ulib_opsc.h"
+#include "ulib_regex.h"
 #include "chuck_io.h"
 
 #if defined(__PLATFORM_WIN32__)
@@ -579,6 +580,8 @@ t_CKBOOL load_internal_modules( Chuck_Compiler * compiler )
     if( !load_module( env, libmath_query, "Math", "global" ) ) goto error;
     EM_log( CK_LOG_SEVERE, "class 'opsc'..." );
     if( !load_module( env, opensoundcontrol_query, "opsc", "global" ) ) goto error;
+    EM_log( CK_LOG_SEVERE, "class 'RegEx'..." );
+    if( !load_module( env, regex_query, "RegEx", "global" ) ) goto error;
     // if( !load_module( env, net_query, "net", "global" ) ) goto error;
     
 #ifndef __DISABLE_MIDI__
