9ace3b08ae208b9d830e37d43a45fead51175d25
Spencer Salazar <spencer@ccrma.stanford.edu>
2013-07-24 05:08:21 +0000

    ckserial Arduino code

---
+++ src/examples/serial/ckserial/ckserial.ino
@@ -0,0 +1,16 @@
+
+char bytes[1024];
+
+void setup()
+{
+  // start serial port at 9600 bps and wait for port to open:
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int nRead = Serial.readBytesUntil('\n', bytes, 1024);
+  if(nRead > 0)
+    Serial.println("bar");
+}
+
