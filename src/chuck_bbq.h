063ced8bc834d7c7e1c08319914a01f16d5c5e09
Spencer Salazar <spencer@ccrma.stanford.edu>
2013-10-30 11:15:50 +0000

    pulse support

--- src/chuck_bbq.h
+++ src/chuck_bbq.h
@@ -39,13 +39,13 @@
 
 #if defined(__LINUX_ALSA__)
 #elif defined(__LINUX_JACK__)
-#elif defined(__LINUX_OSS__)
+#elif defined(__LINUX_PULSE__)
 #elif defined(__MACOSX_CORE__)
 #elif defined(__WINDOWS_DS__)
 #elif defined(__WINDOWS_ASIO__)
 #else
 #error "must define one:\
-__LINUX_ALSA__ __LINUX_JACK__ __LINUX_OSS__ __MACOSX_CORE__ __WINDOWS_DS__"
+__LINUX_ALSA__ __LINUX_JACK__ __LINUX_PULSE__ __MACOSX_CORE__ __WINDOWS_DS__"
 #endif
 
 struct Chuck_VM;
