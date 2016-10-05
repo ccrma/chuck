eb94d9126a5e73f7099ea2438c376e0a262a9a4f
Spencer Salazar <spencer.salazar@gmail.com>
2016-10-04 21:15:45 -0700

    chuck_dl version bump pt 2

--- src/chuck_dl.h
+++ src/chuck_dl.h
@@ -47,7 +47,7 @@
 // major version must be the same between chuck:chugin
 #define CK_DLL_VERSION_MAJOR (0x0006)
 // minor version of chugin must be less than or equal to chuck's
-#define CK_DLL_VERSION_MINOR (0x0001)
+#define CK_DLL_VERSION_MINOR (0x0000)
 #define CK_DLL_VERSION_MAKE(maj,min) ((t_CKUINT)(((maj) << 16) | (min)))
 #define CK_DLL_VERSION_GETMAJOR(v) (((v) >> 16) & 0xFFFF)
 #define CK_DLL_VERSION_GETMINOR(v) ((v) & 0xFFFF)
