8b5fbb288c92da596af5c88ee7817e4a69fff976
Spencer Salazar <spencer.salazar@gmail.com>
2020-04-14 20:33:25 -0700

    compile fix

--- src/core/chuck_win32.c
+++ src/core/chuck_win32.c
@@ -4050,7 +4050,7 @@ long htol( c_str str )
  * down here because we want the user's section 1 to have been scanned first.
  * The user has a chance to override it with an option.
  */
-#include <unistd.h>
+//#include <unistd.h>
 #endif
 
 #ifndef YY_EXTRA_TYPE
