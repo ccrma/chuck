eb083ac5861607de07377aa94757c394d73325e8
Ge Wang <ge@ccrma.stanford.edu>
2022-05-17 11:22:27 -0700

    going for it 1.4.1.1

--- src/core/chuck.cpp
+++ src/core/chuck.cpp
@@ -92,7 +92,7 @@
 
 
 // chuck statics
-const char ChucK::VERSION[] = "1.4.1.1-rc1 (numchucks)";
+const char ChucK::VERSION[] = "1.4.1.1 (numchucks)";
 t_CKUINT ChucK::o_numVMs = 0;
 t_CKBOOL ChucK::o_isGlobalInit = FALSE;
 t_CKBOOL ChucK::enableSystemCall = FALSE;
