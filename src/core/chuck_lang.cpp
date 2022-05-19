86bc2148bb9aa864e1ab0bf84032316a64afbc3e
nshaheed <nicholasshaheed@gmail.com>
2022-05-18 19:19:44 -0700

    removed tabs from .cpp files

--- src/core/chuck_lang.cpp
+++ src/core/chuck_lang.cpp
@@ -2489,16 +2489,20 @@ CK_DLL_MFUN( array_pop_out )
 // (1.4.1.1 nshaheed) array.getKeys()
 CK_DLL_MFUN( array_get_keys )
 {
-	Chuck_Array * array = (Chuck_Array *)SELF;
-	Chuck_Array4 * returned_keys = (Chuck_Array4 *) GET_NEXT_OBJECT(ARGS);
+    Chuck_Array * array = (Chuck_Array *)SELF;
+    Chuck_Array4 * returned_keys = (Chuck_Array4 *) GET_NEXT_OBJECT(ARGS);
 
-	returned_keys->set_size(0);
-	std::vector<std::string> array_keys = array->get_keys();
+    // clear return array
+    returned_keys->set_size(0);
+    // local keys array
+    std::vector<std::string> array_keys;
+    // get the keys
+    array->get_keys( array_keys );
 
-	// copy array keys into the provided string array
-	for (auto const& member : array_keys) {
-	  Chuck_String * key = (Chuck_String *) instantiate_and_initialize_object(SHRED->vm_ref->env()->t_string, SHRED);
-	  key->set(member);
-	  returned_keys->push_back((t_CKUINT) key);
-	}
+    // copy array keys into the provided string array
+    for (t_CKUINT i = 0; i < array_keys.size(); i++ ) {
+        Chuck_String * key = (Chuck_String *)instantiate_and_initialize_object(SHRED->vm_ref->env()->t_string, SHRED);
+        key->set(array_keys[i]);
+        returned_keys->push_back((t_CKUINT) key);
+    }
 }
