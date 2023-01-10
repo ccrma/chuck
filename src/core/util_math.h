0cc756d9270cd6479578fd893c9874fcf5109855
Ge Wang <ge@ccrma.stanford.edu>
2023-01-09 20:28:18 -0800

    added snprintf define for legacy windows

--- src/core/util_math.h
+++ src/core/util_math.h
@@ -52,7 +52,7 @@ double ck_remainder( double a, double b );
 
 
 // max for random functions
-#ifdef __WINDOWS_DS__
+#ifdef __PLATFORM_WIN32__
 #define CK_RANDOM_MAX RAND_MAX
 #else
 #define CK_RANDOM_MAX 0x7fffffff
