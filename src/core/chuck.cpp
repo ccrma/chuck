b600e346980abaf11722c3bcc3439c94e51b3eac
Ge Wang <ge@ccrma.stanford.edu>
2023-06-22 14:06:37 -0700

    update auto-load chuck message

--- src/core/chuck.cpp
+++ src/core/chuck.cpp
@@ -778,7 +778,7 @@ void ChucK::probeChugins()
 
     // check
     if( ck_libs_to_preload.size() == 0 )
-        EM_log( CK_LOG_SYSTEM, "(none found)" );
+        EM_log( CK_LOG_SYSTEM, "(no auto-load chuck files found)" );
 
     // pop log
     EM_poplog();
