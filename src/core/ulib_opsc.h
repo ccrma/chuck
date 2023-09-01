3df84013d14ca4d56500cb0161d346f8bd78c21b
Ge Wang <ge@ccrma.stanford.edu>
2023-09-01 14:16:51 -0700

    fix auto-port support; code reformat OSC; comments

--- src/core/ulib_opsc.h
+++ src/core/ulib_opsc.h
@@ -37,41 +37,8 @@
 #include "chuck_def.h"
 #include "chuck_dl.h"
 
+// Open Sound Control chuck API query entry point
 DLL_QUERY opensoundcontrol_query ( Chuck_DL_Query* query );
 
-CK_DLL_CTOR ( osc_send_ctor );
-CK_DLL_DTOR ( osc_send_dtor );
-CK_DLL_MFUN ( osc_send_setHost );
-CK_DLL_MFUN ( osc_send_startMesg );
-CK_DLL_MFUN ( osc_send_startMesg_spec );
-CK_DLL_MFUN ( osc_send_addInt );
-CK_DLL_MFUN ( osc_send_addFloat );
-CK_DLL_MFUN ( osc_send_addString );
-CK_DLL_MFUN ( osc_send_openBundle );
-CK_DLL_MFUN ( osc_send_closeBundle );
-CK_DLL_MFUN ( osc_send_holdMesg );
-CK_DLL_MFUN ( osc_send_kickMesg );
-
-CK_DLL_CTOR ( osc_address_ctor );
-CK_DLL_DTOR ( osc_address_dtor );
-CK_DLL_MFUN ( osc_address_set );
-CK_DLL_MFUN ( osc_address_can_wait );
-CK_DLL_MFUN ( osc_address_has_mesg );
-CK_DLL_MFUN ( osc_address_next_mesg );
-CK_DLL_MFUN ( osc_address_next_int );
-CK_DLL_MFUN ( osc_address_next_float );
-CK_DLL_MFUN ( osc_address_next_string );
-
-CK_DLL_CTOR ( osc_recv_ctor );
-CK_DLL_DTOR ( osc_recv_dtor );
-CK_DLL_MFUN ( osc_recv_port );
-CK_DLL_MFUN ( osc_recv_new_address );
-CK_DLL_MFUN ( osc_recv_new_address_type );
-CK_DLL_MFUN ( osc_recv_add_address );
-CK_DLL_MFUN ( osc_recv_remove_address );
-CK_DLL_MFUN ( osc_recv_listen );
-CK_DLL_MFUN ( osc_recv_listen_port );
-CK_DLL_MFUN ( osc_recv_listen_stop );
 
 #endif
-
