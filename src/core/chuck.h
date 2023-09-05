b356b748d95d08b87d110f9705217de28268f046
Ge Wang <ge@ccrma.stanford.edu>
2023-09-05 01:03:52 -0700

    begin 1.5.1.4-dev cycle

--- src/core/chuck.h
+++ src/core/chuck.h
@@ -74,7 +74,7 @@
 
 // ChucK version string -- retrieve using ChucK::version()
 // 1.5.0.0 (ge) | moved here for at-a-glance visibility (e.g., for chugins)
-#define CHUCK_VERSION_STRING                    "1.5.1.3 (chai)"
+#define CHUCK_VERSION_STRING                    "1.5.1.4-dev (chai)"
 
 // ChucK param names -- used in setParam(...) and getParam*(...)
 #define CHUCK_PARAM_VERSION                     "VERSION"
