a7f99b427357034dea0df83adbe443ef3b6de2a4
Ge Wang <ge@ccrma.stanford.edu>
2024-01-25 15:03:25 -0800

    ulib_math replace <limits.h> with <limits>

--- src/core/ulib_math.cpp
+++ src/core/ulib_math.cpp
@@ -38,9 +38,9 @@
 #include <float.h>
 #include <stdlib.h>
 #include <time.h>
-#include <limits.h>
 
 #include <vector> // 1.5.0.0 (ge) | added
+#include <limits> // 1.5.2.2 (nick) | replaced <limits.h>
 
 
 static double g_pi = CK_ONE_PI;
